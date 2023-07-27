51ddaee0535c3dcd24f30240493ba2aa4e2c1e86
Wallace Queiros <67396393+WallaceQueiros@users.noreply.github.com>
2023-07-27 15:29:46 -0300

    - Creating of the role marking task as completed.
    - Try except to handle an error that could happen during execution
    
    - Also added file reading, the for loop and condition structures
    
    - Added file writing mode, performing all file rewriting

--- to_do_list.py
+++ to_do_list.py
@@ -19,3 +19,29 @@ def add_task():
     with open("tasks.txt", "a") as file:
         file.write(description + "\n")
     print("Task added successfully!")
+
+
+def mark_task_completed():
+    try:
+        with open("tasks.txt", "r") as file:
+            tasks = file.readline()
+
+        if not tasks:
+            print("No tasks to mark as completed.")
+            return
+
+        print("Task List: ")
+        for idx, task in enumerate(tasks, 1):
+            print(f"{idx}. {task.strip()}")
+
+        task_number = int(input("Enter the task number to mark as completed: "))
+        if 1 <= task_number <= len(tasks):
+            tasks[task_number - 1] = "[Completed] " + tasks[task_number - 1]
+            with open("tasks.txt", "w") as file:
+                file.writelines(tasks)
+            print("Task marked as completed.")
+        else:
+            print("Invalid task number.")
+    except FileNotFoundError:
+        print("No tasks registered yet.")
+
