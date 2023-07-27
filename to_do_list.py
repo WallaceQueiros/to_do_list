42115d1ec1b8352058c8c5f37f9b3335cf23c4a4
Wallace Queiros <67396393+WallaceQueiros@users.noreply.github.com>
2023-07-27 12:59:42 -0300

    Creating the add task function

--- to_do_list.py
+++ to_do_list.py
@@ -9,3 +9,13 @@ def display_menu():
     print("2. Mark task as completed")
     print("3. Display task list")
     print("4. Exit")
+
+
+# Function responsible for adding tasks
+def add_task():
+    description = input("Enter the task description: ")
+# Opens the file "tasks.txt" in writing mode "a"-(append) using the management context "with
+# "a" mode allows adding new lines at the end of the file without overwriting existing content
+    with open("tasks.txt", "a") as file:
+        file.write(description + "\n")
+    print("Task added successfully!")
