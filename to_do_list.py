2f437120e75dfdad0bef8e6dd2f2ee8c60c22b89
Wallace Queiros <67396393+WallaceQueiros@users.noreply.github.com>
2023-07-27 12:08:34 -0300

    Import from the os library and create the display menu function

---
+++ to_do_list.py
@@ -0,0 +1,11 @@
+# This library allows you to interact with the operating system
+import os
+
+
+# Function responsible for displaying the menu
+def display_menu():
+    print("Menu")
+    print("1. Add task")
+    print("2. Mark task as completed")
+    print("3. Display task list")
+    print("4. Exit")
